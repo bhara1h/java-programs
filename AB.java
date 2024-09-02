b2fe0e3caad34a19700d6fe9b75e07a9779e2597
bhara1h <bharathm2269@gmail.com>
2024-09-02 10:39:03 +0530

    Add files via upload

---
+++ AB.java
@@ -0,0 +1,36 @@
+class A {
+    int a, b, c;
+
+    void get(int x) {
+        a = x;
+        b = x;
+    }
+
+    void cal() {
+        c = a + b;
+        System.out.println("Addition1: " + c);
+    }
+}
+
+class B extends A {
+    int d, e;
+
+    void get(int z) {
+        super.get(z);
+        d = z;
+    }
+
+    void cal() {
+        super.cal();
+        e = a + b + c + d;
+        System.out.println("Addition2: " + e);
+    }
+}
+
+public class AB {
+    public static void main(String args[]) {
+        B obj = new B();
+        obj.get(10);
+        obj.cal();
+    }
+}
